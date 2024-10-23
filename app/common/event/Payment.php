85956cdffaad9b62a21852c502b0ef3b4658bd6c
arron1118 <arron1118@icloud.com>
2024-10-23 11:22:44 +0800

    update

--- app/common/event/Payment.php
+++ app/common/event/Payment.php
@@ -21,29 +21,33 @@ class Payment
         $notpay = \app\common\model\Payment::where($where)->select();
         foreach ($notpay as $key => $value) {
             if ($value->pay_type === 1) {
-                // 检查微信订单是否已支付
-                $data = Pay::wechat(Config::get('payment'))
-                    ->query([
-                        'out_trade_no' => $value->payno,
-                        '_action' => 'native', // 查询扫码支付订单
-                    ]);
-                Log::info('微信订单：' . json_encode($data));
-                if ($data->trade_state === 'SUCCESS') {
-                    $mt = mktime(
-                        substr($data->success_time, 8, 2),
-                        substr($data->success_time, 10, 2),
-                        substr($data->success_time, 12, 2),
-                        substr($data->success_time, 4, 2),
-                        substr($data->success_time, 6, 2),
-                        substr($data->success_time, 0, 4)
-                    );
-                    $value->pay_time = $mt;
-                    $value->payment_no = $data->transaction_id;
-                    $value->status = 1;
-                    $value->save();
-                } elseif ($data->trade_state === 'CLOSED') {
-                    $value->status = 2;
-                    $value->save();
+                try {
+                    // 检查微信订单是否已支付
+                    $data = Pay::wechat(Config::get('payment'))
+                        ->query([
+                            'out_trade_no' => $value->payno,
+                            '_action' => 'native', // 查询扫码支付订单
+                        ]);
+                    Log::info('微信订单：' . json_encode($data));
+                    if ($data->trade_state === 'SUCCESS') {
+                        $mt = mktime(
+                            substr($data->success_time, 8, 2),
+                            substr($data->success_time, 10, 2),
+                            substr($data->success_time, 12, 2),
+                            substr($data->success_time, 4, 2),
+                            substr($data->success_time, 6, 2),
+                            substr($data->success_time, 0, 4)
+                        );
+                        $value->pay_time = $mt;
+                        $value->payment_no = $data->transaction_id;
+                        $value->status = 1;
+                        $value->save();
+                    } elseif ($data->trade_state === 'CLOSED') {
+                        $value->status = 2;
+                        $value->save();
+                    }
+                } catch (Exception $e) {
+                    Log::error('微信订单异常：' . json_encode($e));
                 }
             } elseif ($value->pay_type === 2) {
                 // 检查支付宝订单是否已支付
@@ -62,7 +66,7 @@ class Payment
                         $value->save();
                     }
                 } catch (Exception $e) {
-                    Log::error($e->raw['alipay_trade_query_response']);
+                    Log::error('支付宝订单异常：' . json_encode($e));
                     $response = $e->raw['alipay_trade_query_response'];
                     if ($response['code'] === '40004' && $response['sub_code'] === 'ACQ.TRADE_NOT_EXIST') {
                         $value->status = 2;
